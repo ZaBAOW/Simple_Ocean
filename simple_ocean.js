c1a7ac8d2422b984474a1ab209dc4cd74c475ac2
Andrew Kadesky <amkadesky@aol.com>
2017-10-26 13:20:43 -0500

    tts now reads all lyrics

--- simple_ocean.js
+++ simple_ocean.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = 'MzY5MTc0MDExMDA5MTcxNDU2.DMUrmw.6J3Mr8sP4YgLAjUyAHc4dinmL-Q';
-const ocean_msg = "OCEAN MAN 🌊 😍 Take me by the hand ✋ lead me to the land that you understand 🙌 🌊 OCEAN MAN 🌊 😍 The voyage 🚲 to the corner of the 🌎 globe is a real trip 👌 🌊 OCEAN MAN 🌊 😍 The crust of a tan man 👳 imbibed by the sand 👍 Soaking up the 💦 thirst of the land 💯";
+const ocean_msg_1 = "OCEAN MAN 🌊 😍 Take me by the hand ✋ lead me to the land that you understand 🙌 🌊 OCEAN MAN 🌊 😍 The voyage 🚲 to the corner of the 🌎 globe is a real trip 👌 🌊 ";
+const ocean_msg_2 = "OCEAN MAN 🌊 😍 The crust of a tan man 👳 imbibed by the sand 👍 Soaking up the 💦 thirst of the land 💯";
+// const ocean_msg_simple = "OCEAN MAN Take me by the hand lead me to the land that you understand OCEAN MAN The voyage to the corner of the globe is a real trip OCEAN MAN The crust of a tan man imbibed by the sand Soaking up the thirst of the land";
 var counter = 0;
 
 
@@ -15,18 +17,21 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-	if(message.content === ">stop"){
-		counter = 2;
-		message.channel.send("Ok I'll stop it :frowning2:");
-	} else if(counter === 0){
-  		message.reply(ocean_msg);
+	if(message.content === "stop?"){
+		counter = 3;
+		message.channel.send("Ok I'll stop it :frowning2:", {tts: true});
+	} else if(message.content === ocean_msg_1 || message.content === ocean_msg_2){
+		return;
+	}else if(counter === 0){
+  		message.channel.send(ocean_msg_1, {tts: true});
+  		message.channel.send(ocean_msg_2, {tts: true});
   		// message.channel.send("?play ocean man");
   		counter++;
-	} else if(counter > 0 && counter < 2 && message.content != ocean_msg){
+	} else if(counter > 0 && counter < 3 && message.content != ocean_msg_1 || ocean_msg_2){
 		counter--;
 	}  else if(message.content === ">start"){
 		counter = 0;
-		message.channel.send("HERE WE GO!!! :ocean: :ocean: :ocean: ");
+		message.channel.send("HERE WE GO!!! :ocean: :ocean: :ocean:", {tts:true});
 	}
 });
 
